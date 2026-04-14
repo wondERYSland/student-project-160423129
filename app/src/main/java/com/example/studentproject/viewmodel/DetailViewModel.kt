402d185333109a262d66e1836d886c9fdad46fe8
wondERYSland <eluisa.sugiono@gmail.com>
2026-04-14 10:43:52 +0700

    DetailViewModel hardcode change progress

--- app/src/main/java/com/example/studentproject/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/studentproject/viewmodel/DetailViewModel.kt
@@ -1,15 +1,22 @@
 package com.example.studentproject.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.RequestQueue
 import com.example.studentproject.model.Student
 
-class DetailViewModel:ViewModel() {
+class DetailViewModel(application: Application): AndroidViewModel(application) {
     val studentLD = MutableLiveData<Student>()
-
+    private val TAG = "volleyTag"
+    private var queue: RequestQueue?=null
     fun fetch() {
-        val student1 = Student("16055","Nonie","1998/03/28","5718444778",
-            "http://dummyimage.com/75x100.jpg/cc0000/ffffff")
-        studentLD.value = student1
+        val url = "https://www.jsonkeeper.com/b/LLMW"
+
+
+//        val student1 = Student("16055","Nonie","1998/03/28","5718444778",
+//            "http://dummyimage.com/75x100.jpg/cc0000/ffffff")
+//        studentLD.value = student1
     }
 }
